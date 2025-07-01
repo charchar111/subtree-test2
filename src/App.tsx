f0f31fe274107cd008efb387842c59477e418ad5
차태영 <3hjab@naver.com>
2025-07-01 15:53:11 +0900

    파일 정리

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  console.log("hello app");
+  console.log("hello app2");
   const [count, setCount] = useState(0);
 
   return (
