ea23183ab1d7c9da1c6d3d2e861169590a25216a
차태영 <3hjab@naver.com>
2025-07-07 16:33:28 +0900

    main 수정5

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  console.log("main 수정4");
+  console.log("main 수정5");
   const [count, setCount] = useState(0);
 
   return (
