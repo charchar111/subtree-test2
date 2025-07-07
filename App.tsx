668b6b12c6e40a45555cbfd973775ee520e039bb
차태영 <3hjab@naver.com>
2025-07-07 13:27:54 +0900

    main 반영

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  console.log("hello app3");
+  console.log("qeqeqeqee2");
   const [count, setCount] = useState(0);
 
   return (
