72b7912ac8fbca9d5be69a2ed974890e05f15f2a
차태영 <3hjab@naver.com>
2025-07-08 11:32:43 +0900

    메타 파일 추가 - tsconfig, package.json

---
+++ src/components/Card.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactLogo from "@assets/react.svg";
+// import ReactLogo from "../assets/react.svg";
+
+export default function Card() {
+  return (
+    <div>
+      Card
+      <div>
+        <img src={ReactLogo} alt="" />
+      </div>
+    </div>
+  );
+}
